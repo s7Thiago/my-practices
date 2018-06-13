cec72af434ae50e793678dcbf0144d39d0a8f8ea
s7thiago <thyagosousasilva@gmail.com>
2018-06-13 19:19:41 -0300

    Adicionando projetos de práticas com streams

---
+++ InputOutputStreams/src/Main.java
@@ -0,0 +1,32 @@
+import java.io.FileInputStream;																														
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Main{
+	
+	public static void main(String[] args) {
+		
+		InputStream inputstream;
+		try {
+			inputstream = new FileInputStream("/storage/emulated/0/AppProjects/Java/Testes Projetos/mFileOperations5/Anderson Vieira@.txt");
+
+			int data = inputstream.read();
+			while (data != -1) {
+				System.out.print(String.valueOf(data));
+
+				data = inputstream.read();
+
+			}
+			inputstream.close();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
