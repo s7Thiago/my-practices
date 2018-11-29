8eb15c3cc1857381082e8a58d11cc44a77f8a7d1
s7thiago <thyagosousasilva@gmail.com>
2018-11-29 13:07:13 -0200

    Melhorando o código

--- Linguagem_C/Exemplo_Entrada_de_dados/main.c
+++ Linguagem_C/Exemplo_Entrada_de_dados/main.c
@@ -7,7 +7,15 @@ int main() {
     int idade;
 
     printf("Digite o primeiro nome: ");
-    gets(nome);
+    //Aqui, poderiamos colocar um gets(nome); mas para impedir que
+    //esta string receba outras coisas alem do primeiro nome. Neste
+    //caso, usaremos  scanf("%s", nome); Este comando por padrao para
+    //a recepcao de dados assi que encontra o primeiro caractere de espaco
+    scanf("%s", &nome);
+
+    //Precisamos sempre limpar o buffer para evitar problemas, como por exemplo,
+    //A proxima entrada ser ignorada.
+    fflush(stdin);
 
     printf("Agora o sobrenome: ");
     //O operador [^\n] diz faz a scanf() parar de pegar a string
@@ -15,6 +23,10 @@ int main() {
     //o comportamento padrao (parar quando encontrar um caractere
     //de espaço);
     scanf("%[^\n]", sobrenome);
+    //O que tambem poderia ser feito nalinha anterior, eh modificar
+    //o numero de caracteres adicionando um numero entre os simbolos
+    //%[ assim: scanf("%100[^\n]", sobrenome);
+    //neste caso, a nossa entrada estaria limitada a 100 caracteres.
 
     printf("E agora a idade: ");
     scanf("%d", &idade);
