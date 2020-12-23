e243cf3a175bc6ad7fb589ced9a716f8ce415141
s7thiago <thyagosousasilva@gmail.com>
2020-12-22 21:48:03 -0300

    Improving some logics

--- Courses/Angular_5_JWT_Spring_Boot_REST_Security_Data_e_MongoDB/2-angular/first-angular5-cli/src/app/task-list/task-list.component.ts
+++ Courses/Angular_5_JWT_Spring_Boot_REST_Security_Data_e_MongoDB/2-angular/first-angular5-cli/src/app/task-list/task-list.component.ts
@@ -9,13 +9,31 @@ export class TaskListComponent {
   title = 'Tasks';
   tasks = [];
   task = ""; // Representa o valor do campo de texto
+  isAdmin = true;
+  isAddingEmptyValue = false;
+  typeSomethingMessage = 'Type something!!!';
 
   // Clique do botão de adicionar tarefa (task)
   add(): void {
+    this.tasks.reverse;
     if (this.task != "") {
-      console.log("Add foi acionado!");
-      this.tasks.push(this.task);
-      this.task = "";
+      this.isAddingEmptyValue = false;
+
+      this.task == this.typeSomethingMessage ? null :
+        this.tasks.push(this.task);
+
+      this.task != this.typeSomethingMessage ?
+        this.task = ""
+        : null;
+
+      this.task == this.typeSomethingMessage ?
+        this.isAddingEmptyValue = true
+        : null;
+
+    } else {
+      this.task = this.typeSomethingMessage
+      this.isAddingEmptyValue = true;
     }
+
   }
 }
